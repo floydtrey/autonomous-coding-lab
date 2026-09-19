085c12d404bb340aaecaeb7e1bebd7cc24be2e27
floydtrey <floydtrey@ymail.com>
2026-09-19 01:26:29 -0500

    roles: export role contract error

--- acl_roles/common/__init__.py
+++ acl_roles/common/__init__.py
@@ -1,5 +1,6 @@
 from .context import ContextReference, RoleContext
 from .diagnostics import RoleDiagnostics
+from .errors import RoleContractError
 from .instructions import InstructionSet
 from .request import RoleRequest
 from .response import RoleResponse, RoleStatus
@@ -8,6 +9,7 @@ __all__ = [
     "ContextReference",
     "InstructionSet",
     "RoleContext",
+    "RoleContractError",
     "RoleDiagnostics",
     "RoleRequest",
     "RoleResponse",
