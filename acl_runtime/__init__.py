eb719ccc1b765309ff73e41bd0d4b50acf8cab61
floydtrey <floydtrey@ymail.com>
2026-09-19 02:14:44 -0500

    runtime: avoid eager runner import

--- acl_runtime/__init__.py
+++ acl_runtime/__init__.py
@@ -1,6 +1,15 @@
-"""Reusable ACL Next runtime entrypoints."""
-from .runner import run_program
+"""Reusable ACL Next runtime entrypoints.
+
+Keep package import side-effect free so `python -m acl_runtime.runner` does not
+pre-import the module it is about to execute.
+"""
 from .services import LocalServiceConfig, LocalServiceSupervisor, ProbeResult, ProbeState
+
+
+def run_program(*args, **kwargs):
+    from .runner import run_program as _run_program
+    return _run_program(*args, **kwargs)
+
 
 __all__ = [
     "LocalServiceConfig",
