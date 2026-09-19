cda7b670bb923b30db9716a2cf2a3ebf2a5e75d6
floydtrey <floydtrey@ymail.com>
2026-09-19 05:53:54 -0500

    authority: export filesystem authority types

--- acl_core/authority/__init__.py
+++ acl_core/authority/__init__.py
@@ -1,4 +1,21 @@
+from .filesystem import (
+    FilesystemAuthorityService,
+    FilesystemDecision,
+    FilesystemOperation,
+    ProtectedPath,
+    ProtectionLayer,
+)
 from .models import AuthorityEnvelope, AuthorityGrant, AuthorityRequest
 from .service import AuthorityService
 
-__all__ = ["AuthorityEnvelope", "AuthorityGrant", "AuthorityRequest", "AuthorityService"]
+__all__ = [
+    "AuthorityEnvelope",
+    "AuthorityGrant",
+    "AuthorityRequest",
+    "AuthorityService",
+    "FilesystemAuthorityService",
+    "FilesystemDecision",
+    "FilesystemOperation",
+    "ProtectedPath",
+    "ProtectionLayer",
+]
