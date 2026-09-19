60613157193625595b1de7c570054e6bc17f55d3
floydtrey <floydtrey@ymail.com>
2026-09-19 06:26:57 -0500

    planner: export consultation interfaces

--- acl_controller/planner/__init__.py
+++ acl_controller/planner/__init__.py
@@ -1,3 +1,14 @@
+from .consultation import (
+    CONSULTATION_CONFIG_SCHEMA,
+    JsonPlannerConsultationStore,
+    PlannerConsultationConfig,
+    PlannerConsultationExchange,
+    PlannerConsultationOutcome,
+    PlannerConsultationOutcomeStatus,
+    PlannerConsultationRecord,
+    PlannerConsultationService,
+    PlannerConsultationStatus,
+)
 from .disposition import (
     PlannerDispositionOutcome,
     PlannerDispositionService,
@@ -6,7 +17,16 @@ from .disposition import (
 from .runtime import ControllerPlannerRuntimeBackend, require_planner_runtime_backend
 
 __all__ = [
+    "CONSULTATION_CONFIG_SCHEMA",
     "ControllerPlannerRuntimeBackend",
+    "JsonPlannerConsultationStore",
+    "PlannerConsultationConfig",
+    "PlannerConsultationExchange",
+    "PlannerConsultationOutcome",
+    "PlannerConsultationOutcomeStatus",
+    "PlannerConsultationRecord",
+    "PlannerConsultationService",
+    "PlannerConsultationStatus",
     "PlannerDispositionOutcome",
     "PlannerDispositionService",
     "PlannerOutcomeStatus",
