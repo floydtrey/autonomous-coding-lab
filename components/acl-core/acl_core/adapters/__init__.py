05de4d04b0a419c33e79899d185649f339942d90
floydtrey <floydtrey@ymail.com>
2026-09-19 00:20:24 -0500

    core: add adapter contracts

---
+++ components/acl-core/acl_core/adapters/__init__.py
@@ -0,0 +1,4 @@
+from .base import AdapterRequest, AdapterResponse, CoreAdapter
+from .registry import AdapterRegistry
+
+__all__ = ["AdapterRequest", "AdapterResponse", "CoreAdapter", "AdapterRegistry"]
