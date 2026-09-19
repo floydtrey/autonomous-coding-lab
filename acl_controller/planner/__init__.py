e057ddbec5f0fd44795f0e791dbe46ee30e1d3e3
floydtrey <floydtrey@ymail.com>
2026-09-19 06:21:40 -0500

    planner: export fast-path disposition service

--- acl_controller/planner/__init__.py
+++ acl_controller/planner/__init__.py
@@ -1,3 +1,14 @@
+from .disposition import (
+    PlannerDispositionOutcome,
+    PlannerDispositionService,
+    PlannerOutcomeStatus,
+)
 from .runtime import ControllerPlannerRuntimeBackend, require_planner_runtime_backend
 
-__all__ = ["ControllerPlannerRuntimeBackend", "require_planner_runtime_backend"]
+__all__ = [
+    "ControllerPlannerRuntimeBackend",
+    "PlannerDispositionOutcome",
+    "PlannerDispositionService",
+    "PlannerOutcomeStatus",
+    "require_planner_runtime_backend",
+]
