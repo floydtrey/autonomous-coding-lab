17fbf2677bef9b3f0a96ef5a53acc6813e99eb7d
floydtrey <floydtrey@ymail.com>
2026-09-19 02:05:04 -0500

    runtime: export local-service supervisor

--- acl_runtime/__init__.py
+++ acl_runtime/__init__.py
@@ -1,4 +1,11 @@
 """Reusable ACL Next runtime entrypoints."""
 from .runner import run_program
+from .services import LocalServiceConfig, LocalServiceSupervisor, ProbeResult, ProbeState
 
-__all__ = ["run_program"]
+__all__ = [
+    "LocalServiceConfig",
+    "LocalServiceSupervisor",
+    "ProbeResult",
+    "ProbeState",
+    "run_program",
+]
