82df4cc5a6a6af1f3e42048f06520f3728f88882
floydtrey <floydtrey@ymail.com>
2026-09-19 20:42:03 -0500

    recovery: export shared participant contract

--- acl_controller/recovery/__init__.py
+++ acl_controller/recovery/__init__.py
@@ -1,3 +1,15 @@
-from .service import JsonStopStore, RecoveryService, StopRecord, StopStatus
+from .service import (
+    JsonStopStore,
+    RecoveryParticipant,
+    RecoveryService,
+    StopRecord,
+    StopStatus,
+)
 
-__all__ = ["JsonStopStore", "RecoveryService", "StopRecord", "StopStatus"]
+__all__ = [
+    "JsonStopStore",
+    "RecoveryParticipant",
+    "RecoveryService",
+    "StopRecord",
+    "StopStatus",
+]
